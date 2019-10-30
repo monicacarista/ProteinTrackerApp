018943d4d8c3cae72f068c9aeea2218ca12f8252
monicacarista <monica.carista@si.ukdw.ac.id>
2019-10-30 19:00:16 +0700

    app

--- app/src/main/java/com/example/proteintrackerapp/MainActivity.java
+++ app/src/main/java/com/example/proteintrackerapp/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.proteintrackerapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button resetButton = (Button)findViewById(R.id.btnReset);
+        //resetButton.setOnClickListener(resetButtonListener);
+
+
+        resetButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Apakah anda yakin untuk mereset nilai protein?")
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this, "Tidak jadi reset",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this, "Melakukan RESET !!",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+
     }
+
+
+
 }
